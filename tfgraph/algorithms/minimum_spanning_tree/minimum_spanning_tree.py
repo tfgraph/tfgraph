d30c1917ac04b6dbaddc5ab2c9e8e4348478ed9e
Sergio Garcia Prado <sergio@garciparedes.me>
2017-09-04 19:08:00 +0200

    Working on MinimumSpanningTree class initialization #7

--- tfgraph/algorithms/minimum_spanning_tree/minimum_spanning_tree.py
+++ tfgraph/algorithms/minimum_spanning_tree/minimum_spanning_tree.py
@@ -1,6 +1,33 @@
+import tensorflow as tf
+
+from tfgraph import TensorFlowObject, Graph
 
 
-class MinimumSpanningTree:
+class MinimumSpanningTree(TensorFlowObject):
+  """
+  Attributes:
+    sess (:obj:`tf.Session`): This attribute represents the session that runs
+      the TensorFlow operations.
+    name (str): This attribute represents the name of the object in TensorFlow's
+      op Graph.
+    graph (:obj:`tfgraph.Graph`): The graph on witch it will be calculated the
+      algorithm. It will be treated as Directed Weighted Graph.
+  """
 
-  def __init__(self):
+  def __init__(self, sess: tf.Session, name: str, graph: Graph):
+    """
+    Constructor of the class.
+
+    Args:
+      sess (:obj:`tf.Session`): This attribute represents the session that runs
+        the TensorFlow operations.
+      name (str): This attribute represents the name of the object in
+        TensorFlow's op Graph.
+      graph (:obj:`tfgraph.Graph`): The graph on witch it will be calculated the
+        algorithm. It will be treated as Directed Weighted Graph.
+
+    """
+    name = name + '_minimum_spanning_tree'
+    TensorFlowObject.__init__(self, sess, name)
+    self.graph = graph
     pass
